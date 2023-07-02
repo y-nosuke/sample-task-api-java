8792c34d7cdbdbb513149f968830334bf974d4fe
nakao yoichi <yoichi@ad.physicist00.jp>
2023-07-03 08:13:48 +0900

    プロジェクト作成

---
+++ src/main/java/jp/physicist00/sampletaskapijava/SampleTaskApiJavaApplication.java
@@ -0,0 +1,13 @@
+package jp.physicist00.sampletaskapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTaskApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTaskApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/physicist00/sampletaskapijava/SampleTaskApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.physicist00.sampletaskapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleTaskApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
